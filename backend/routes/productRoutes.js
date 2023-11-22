9921ea05e184ec38e93499da00e869ae7cbd8a68
Brooksaranyapong <brook.saranyapong@gmail.com>
2023-11-22 21:32:32 +0700

    update Backend

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+const router = express.Router();
+import {
+  getProducts,
+  getProductById,
+} from "../controller/productController.js";
+
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
+
+export default router;
