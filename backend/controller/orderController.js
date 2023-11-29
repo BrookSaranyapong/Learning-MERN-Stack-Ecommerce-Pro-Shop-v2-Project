112ac8f97da8ab80ef1d3eeff6c8c15bd26484d1
Brooksaranyapong <brook.saranyapong@gmail.com>
2023-11-29 20:22:29 +0700

    add Create Get Orders

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -1,25 +1,65 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import Order from "../models/orderModel.js";
 
+// @desc    Create new order
+// @route   POST /api/orders
+// @access  Private
+const addOrderItems = asyncHandler(async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethods,
+    itemsPrice,
+    taxPrice,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No Orders items");
+  } else {
+    const order = new Order({
+      orderItems: orderItems.map((x) => ({
+        ...x,
+        product: x._id,
+        _id: undefined,
+      })),
+      user: req.user._id,
+      shippingAddress,
+      paymentMethods,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+    const createOrder = await order.save();
+
+    res.status(201).json(createOrder);
+  }
+});
+
 // @desc    Get Logged in user orders
 // @route   GET /api/orders/myorders
 // @access  Private
 const getMyOrders = asyncHandler(async (req, res) => {
-  res.send("get my orders");
+  const orders = await Order.find({ user: req.user._id });
+  res.status(200).json(orders);
 });
 
 // @desc    Get order by ID
 // @route   GET /api/orders/:id
 // @access  Private
 const getOrderById = asyncHandler(async (req, res) => {
-  res.send("get order by id");
-});
-
-// @desc    Create new order
-// @route   POST /api/orders
-// @access  Private
-const addOrderItems = asyncHandler(async (req, res) => {
-  res.send("add order items ");
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+  if (order) {
+    res.status(200).json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
 });
 
 // @desc    Update order to paid
