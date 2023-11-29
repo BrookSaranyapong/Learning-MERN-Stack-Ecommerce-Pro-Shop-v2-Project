c98d3091a39acf8cf9dd2db19b785e83cf6d6f6c
Brooksaranyapong <brook.saranyapong@gmail.com>
2023-11-29 20:12:35 +0700

    add Backend Order Routes Controller

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import connectDB from "./config/db.js";
 
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoute.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 // use Dotenv
@@ -29,6 +30,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
