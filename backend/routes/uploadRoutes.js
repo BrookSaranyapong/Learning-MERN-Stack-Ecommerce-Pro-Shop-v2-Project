56f427da7b2cdf28d14e664f341f969512d91cce
Brooksaranyapong <brook.saranyapong@gmail.com>
2023-12-10 00:16:53 +0700

    add Multer Image Upload Endpoint

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -17,9 +17,7 @@ const storage = multer.diskStorage({
 
 function checkFileType(file, cb) {
   const filetypes = /jpg|jpeg|png/;
-  const extname = filetypes.test(
-    path.extname(file.originalname).toLocaleLowerCase()
-  );
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
 
   const mimetype = filetypes.test(file.mimetype);
   if (extname && mimetype) {
