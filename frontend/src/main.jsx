f8fb0e82ebf26f4acf6f4e23eabf0b9d0c97e1b8
Brooksaranyapong <brook.saranyapong@gmail.com>
2023-12-09 20:58:29 +0700

    add List Products For Admin

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -26,6 +26,7 @@ import PlaceOrderScreen from "./screens/PlaceOrderScreen.jsx";
 import OrderScreen from "./screens/OrderScreen.jsx";
 import ProfileScreen from "./screens/ProfileScreen.jsx";
 import OrderListScreen from "./screens/admin/OrderListScreen.jsx";
+import ProductListScreen from "./screens/admin/ProductListScreen.jsx";
 
 const router = createBrowserRouter(
   // Nested Route
@@ -47,6 +48,7 @@ const router = createBrowserRouter(
 
       <Route path="" element={<AdminRoute />}>
         <Route path="/admin/orderlist" element={<OrderListScreen />} />
+        <Route path="/admin/productlist" element={<ProductListScreen />} />
       </Route>
     </Route>
   )
