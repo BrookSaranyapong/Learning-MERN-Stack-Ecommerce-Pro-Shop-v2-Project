d9e354003e817196032f6ab9d09194d369745fe3
Brooksaranyapong <brook.saranyapong@gmail.com>
2023-12-10 23:13:45 +0700

    add List Users For Admin

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -28,6 +28,8 @@ import ProfileScreen from "./screens/ProfileScreen.jsx";
 import OrderListScreen from "./screens/admin/OrderListScreen.jsx";
 import ProductListScreen from "./screens/admin/ProductListScreen.jsx";
 import ProductEditScreen from "./screens/admin/ProductEditScreen.jsx";
+import UserListScreen from "./screens/admin/UserListScreen.jsx";
+
 const router = createBrowserRouter(
   // Nested Route
   createRoutesFromElements(
@@ -50,6 +52,7 @@ const router = createBrowserRouter(
         <Route path="/admin/orderlist" element={<OrderListScreen />} />
         <Route path="/admin/productlist" element={<ProductListScreen />} />
         <Route path="/admin/product/:id/edit" element={<ProductEditScreen />} />
+        <Route path="/admin/userlist" element={<UserListScreen />} />
       </Route>
     </Route>
   )
